4ccfca61965a4d4bc1b75a265ff55cb313d4e82d
沛 <banner25387@users.noreply.github.com>
2026-01-14 17:20:22 +0800

    vault backup: 2026-01-14 17:20:22

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -26,7 +26,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.TagList(),
   ],
   left: [
-    Component.PageTitle({ homePageUrl: "/" }),
+    Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Flex({
       components: [
