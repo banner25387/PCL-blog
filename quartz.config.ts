9b4a05282d74b2a8e7437c68b85af82423ac7d9b
沛 <banner25387@users.noreply.github.com>
2026-01-14 16:43:05 +0800

    vault backup: 2026-01-14 16:43:05

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "banner25387.github.io/PCL-blog",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
