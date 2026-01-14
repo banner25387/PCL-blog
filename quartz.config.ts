decc9d4ddc5aff276c524f121b5b97d2b0e89a91
D10429002_Pei Chi Li <52564126+banner25387@users.noreply.github.com>
2026-01-14 13:04:09 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "PCL's blog",
+    pageTitle: "Espresso",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
